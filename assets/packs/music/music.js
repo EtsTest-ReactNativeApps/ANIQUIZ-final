355cadd2e343e5ffb3dcc5df3afefbe8a9ed6e6e
NC <sangwulee2@gmail.com>
2017-12-30 06:48:45 +0900

    standard finished, adding music

--- assets/packs/music/music.js
+++ assets/packs/music/music.js
@@ -1,3 +1,18 @@
 export const MUSIC = [
-	require('./1.mp3')
+	require('./1.mp3'),
+	require('./2.mp3'),
+	require('./3.mp3'),
+	require('./4.mp3'),
+	require('./5.mp3'),
+	require('./6.mp3'),
+	require('./7.mp3'),
+	require('./8.mp3'),
+	require('./9.mp3'),
+	require('./10.mp3'),
+	require('./11.mp3'),
+	require('./12.mp3'),
+	require('./13.mp3'),
+	require('./14.mp3'),
+	require('./15.mp3'),
+	require('./16.mp3')
 ];
